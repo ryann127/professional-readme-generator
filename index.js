804eca1822f1131054b5528c78bbc040a89cc5da
ryann127 <ryann.goldberg127@gmail.com>
2022-04-21 16:41:33 -0600

    added necessary folders and files. Created code for node questions.

---
+++ index.js
@@ -0,0 +1,90 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "Please enter a one-sentence description of your project:",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "Please tell us about your project:",
+        name: "about",
+    },
+    {
+        type: "input",
+        message: "What are the installation instructions for your project?",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "What is the link to clone the repo?",
+        name: "clone",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please select the license you used for this project.",
+        choices: [
+            "GNU AGPLv3",
+            "GNU GPLv3",
+            "GNU LGPLv3",
+            "Mozilla",
+            "MIT",
+            "Apache",
+            "Boost",
+        ],
+    },
+    {
+        type: "input",
+        message: "Please enter any testing protocols you used for your project?",
+        name: "test",
+    },
+    {
+        type: "input",
+        name: "author",
+        message: "What is your name?",
+    },
+    {
+        type: "input",
+        name: "userName",
+        message: "What is your GitHub username?",
+    },
+    {
+        type: "input",
+        name: "userEmail",
+        message: "What is your email?",
+    },
+    {
+        type: "input",
+        name: "URL",
+        message: "What is the URL of the live site?",
+    },
+    {
+        type: "input",
+        name: "repo",
+        message: "What is the URL of the github repo?",
+    },
+];
+
+// TODO: Create a function to write README file
+// function writeToFile(fileName, data) { }
+
+function licenseBadge(value) {
+}
+
+// TODO: Create a function to initialize app
+async function init() {
+
+}
+
+// Function call to initialize app
+init();
